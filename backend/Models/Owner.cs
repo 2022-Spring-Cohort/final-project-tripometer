162fb896fa5e5da80ac9407d48438246bb162526
Darius Hammond <hammond.work.business@gmail.com>
2022-03-29 13:37:37 -0400

    Owner

---
+++ backend/Models/Owner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TripometerAPI.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }   
+}
