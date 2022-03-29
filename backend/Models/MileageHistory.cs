7e5c0ef956389d811c4a09b192cea95e664e8b11
Denzale McIntyre <denzale.mcintyre@aol.com>
2022-03-29 13:41:17 -0400

    updated

--- backend/Models/MileageHistory.cs
+++ backend/Models/MileageHistory.cs
@@ -1,6 +1,19 @@
-﻿namespace TripometerAPI.Models
+﻿using System;
+
+namespace TripometerAPI.Models
 {
     public class MileageHistory
-    {
+    { public int Id { get; set; }
+
+        public int VehicleId { get; set; }
+
+        public string Model { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Mileage { get; set; }
+
+        public string Make { get; set; }
+
     }
 }
--- backend/Models/Vehicle.cs
+++ backend/Models/Vehicle.cs
@@ -1,6 +1,20 @@
-﻿namespace TripometerAPI.Models
+﻿using System.Collections.Generic;
+
+namespace TripometerAPI.Models
 {
     public class Vehicle
     {
+        public int Id { get; set; }
+
+        public string Model { get; set; }
+
+        public int FuelEfficiency { get; set; }
+
+        public string Make { get; set; }
+
+        public string Year { get; set; }
+
+        public virtual List <MileageHistory> History {get;set;}
+
     }
 }
