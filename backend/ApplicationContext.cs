c3b1a4e5993a3d56a60a91ff6663a6776ed03fa0
Darius Hammond <hammond.work.business@gmail.com>
2022-03-30 11:23:21 -0400

    contoller

---
+++ backend/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TripometerAPI.Models;
+
+namespace TripometerAPI
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Owner> Owner { get; set; }
+      
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database = BlogRun; Trusted_connection=True");
+            builder.UseLazyLoadingProxies();
+        }
+
+
+
+    }
+}
---
+++ backend/Controllers/OwnerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TripometerAPI.Models;
+
+namespace TripometerAPI.Controllers
+{
+    public class OwnerController : Controller
+    {
+        private ApplicationContext _context;
+
+        public OwnerController()
+        {
+            _context = new ApplicationContext();
+        }
+
+        [HttpGet]
+
+        public IEnumerable<Owner> get()
+        {
+            return _context.Owner.ToList();
+
+        }
+    }
+}
