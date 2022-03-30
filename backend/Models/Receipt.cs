c91eacd67968617d38708974bc8567418483b608
Denzale McIntyre <denzale.mcintyre@aol.com>
2022-03-30 10:16:38 -0400

    fixed model

--- backend/Models/Receipt.cs
+++ backend/Models/Receipt.cs
@@ -2,5 +2,18 @@
 {
     public class Receipt
     {
+        public int Id { get; set; }
+
+        public int Date { get; set; }
+
+        public int PricePerGallon { get; set; }
+
+        public int TotalCost { get; set; }
+
+        public int TripCost { get; set; }
+
+        public string GasStation { get; set; }
+
+
     }
 }
