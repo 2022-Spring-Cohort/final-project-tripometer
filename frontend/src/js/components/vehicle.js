c3671d7f9daf8a1e182562a76e812b598c10d69f
Darius Hammond <hammond.work.business@gmail.com>
2022-04-11 10:06:54 -0400

    not ready

--- frontend/src/js/components/vehicle.js
+++ frontend/src/js/components/vehicle.js
@@ -136,11 +136,34 @@ function  SumbitEditVehicle(EditVehicle){
 
 
 function AddVehicle() {
+    (async () => {
+        const response = await fetch(
+            'https://parseapi.back4app.com/classes/Carmodels_Car_Model_List?limit=10',
+            {
+                headers: {
+                    'X-Parse-Application-Id': 'iqVulf4rcnwk8v3YC71FGKNkUrY7mzNJosyQzpUA', // This is your app's application id
+                    'X-Parse-REST-API-Key': 'ugWIeOKk3FQoATqdZaR7Gv4jTsc2tsFzLjDNST5a', // This is your app's REST API key
+                }
+            }
+        );
+        const data = await response.json(); // Here you have the data that you need
+        console.log(data);
+        console.log(data.results);
+        console.log(data.results.length);
+        for (let i = 0; i < data.results.length; i++) {
+           console.log(data.results[i].Make);
+        // console.log(data.results[i].Model);
+        // console.log(data.results[i].Year);
+       
+    
+
     appDiv.innerHTML = `
     <h2>Add Vehicle</h2>
 
     <label for="Make ">Make of vehicle</label>
-    <input type="text" id="Make"> 
+    <select id="MakeVehicle">
+    <option id="Make" value="${data.results[i].Make}">${data.results[i].Make}</option>
+    </select>
 
     <label for="Model">Model of vehicle</label>
     <input type="text" id="Model">
@@ -157,6 +180,28 @@ function AddVehicle() {
     <button type="submit" id="createNewVehicleBtn">Submit</button>
 `;
 }
+})();
+}
+
+/**
+ * <label for="Trip">Trip</label>
+        <select id="tripId">
+            ${trips.map(t=>{
+                return `
+                    <option value="${t.id}">From ${t.startAddress} To ${t.endAddress}</option>
+                `
+            }).join('')}
+            <option selected disabled>Please select a trip</option>
+        </select>
+
+
+<option value="1988">1988</option>
+
+    <select id="Make">
+    <option value="${t.id}">From ${t.startAddress} To ${t.endAddress}</option>
+    </select>
+ */
+
 
 
 
@@ -207,6 +252,12 @@ SumbmitButton.addEventListener('click',function(){
         }
     );
     const data = await response.json(); // Here you have the data that you need
-    console.log(JSON.stringify(data, null, 2));
-   // console.log(data.)
+    console.log(data);
+    console.log(data.results);
+    console.log(data.results.length);
+    for (let i = 0; i < data.results.length; i++) {
+    // console.log(data.results[i].Make);
+    // console.log(data.results[i].Model);
+    // console.log(data.results[i].Year);
+    }
 })();
