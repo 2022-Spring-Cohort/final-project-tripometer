f78d93596d9647290f82fe26ebf9a41bf47289a0
Carlos Lopez <carlos@wecancodeit.org>
2022-04-12 11:00:03 -0400

    Added Users endpoint that offers registration and authentication

---
+++ backend/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace TripometerAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        [JsonIgnore]
+        public string PasswordHash { get; set; }
+
+    }
+}
