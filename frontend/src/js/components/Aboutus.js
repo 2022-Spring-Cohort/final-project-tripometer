34114627db7d18e5475bcbea368e8853c3e3c3e5
Darius Hammond <hammond.work.business@gmail.com>
2022-04-13 09:57:59 -0400

    not done

--- frontend/src/js/components/Aboutus.js
+++ frontend/src/js/components/Aboutus.js
@@ -1,15 +1,30 @@
 
-export default{
+export default {
     SetupFooter
 }
 
 const appDiv = document.getElementById("app");
 
-function SetupFooter(){
- 
+function SetupFooter() {
+
 
     appDiv.innerHTML = `
         <div>
+
+        <h2>Inspiration for our project</h2>
+
+        <p> Over the years gas prices have risen to unseen highs which affect where we go, how often, and what we are able to experience during the adventure</p>
+
+        <p>The idea of our project was to be able to plan a trip from one destination to another returning an estimate of..</p>
+
+        <li>-Where and when this trip took place</li>
+        <li>-How much gas would be needed</li>
+        <li>-The total cost of the gas needed for the trip</li>
+        <li>-How many miles were driven during the trip</li>
+        <li>-How many stops to refill at gas stations would be needed and where they are</li>
+        <li>-And finally to record and print the details of the trip</li>
+
+        <p>The data for the trip is then stored and recorded in the form of receipts for later use. While also keeping track of the trips taken, where the driver went and what type of car was driven.</p>
         
         <h1>Here is a list of our project members with links to either their respective Linkden or Github profiles </h1>
         
@@ -22,7 +37,13 @@ function SetupFooter(){
        <ul><a href="https://www.linkedin.com/in/rimma-girsheva" target="_blank">Rimma's Linken</a> </ul>
         </section>
 
-            
+        <h3>Special Thanks</h3>
+
+        <li>Carlos Lopez- Lead Coding Instructor </li>
+        <li>Galvin Hensley- Coding Instructor / Exit Ticket Man </li>
+        <li>Kevin McDonald- Career Advancement Advisor</li>
+        <li>Staff of the WCCI program</li>
+                    
         </div>
     `;
 }
